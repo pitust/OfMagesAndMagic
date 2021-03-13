c61d809b543a06369648c4e532e11b8b44136a4f
pitust <piotr@stelmaszek.com>
2021-03-13 12:45:07 +0000

    fhqwads pushed!
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- teams/fhqwads/mage.py
+++ teams/fhqwads/mage.py
@@ -1,14 +1,18 @@
 import sys
+images = sys.modules['app.resources.images']
+run = sys.modules['__main__']
+images.ImageManager().images['ac_mage'] = images.ImageManager().images['lightning_bolt']
 magic = sys.modules['app.models.magic']
 class Etable:
     def __contains__(self, b):
         return True
 print("""
-       /-------  /    /   __    |    /\        / ADS
-      /         /    /  /   \   |   /  \      /
-     /-----    /----/  /     \  |  /    \    /
-    /         /    /  /      /  | /      \  /
-   /         /    /    \----/   |/        \/
+                                                                     _____
+       /-------  /    /   __    |    /\        /  /\\\\         /\   /
+      /         /    /  /   \   |   /  \      /  /  \\\\       /  |  \ 
+     /-----    /----/  /     \  |  /    \    /  /----\\\\     /   /   \ 
+    /         /    /  /      /  | /      \  /  /      \\\\   /   /     |
+   /         /    /    \----/   |/        \/  /        \\\\ /___/  ____/
                              \\
 
  == REDEFINING MAGIC ==
@@ -40,22 +44,19 @@ class Mage:
         self.name = f"fhqwads mage #{idx}"
         self.element = "AC"
 
-        if idx % 2 == 0:
-            self.type = 'dd'
-            self.health  = 50
-            self.attack  = 20
-            self.defense = 20
-            self.speed   = 10
-            self.spells = [ "Shattering Bomb" ]
-        if idx % 2 == 1:
-            self.type = 'reducer'
-            self.health  = 25 
-            self.attack  = 0
-            self.defense = 0
-            self.speed   = 75
-            self.spells = [ "Shattering Bomb" ]
+        self.health  = 30
+        self.attack  = 30
+        self.defense = 30
+        self.speed   = 10
+        self.spells = [ "Shattering Bomb" ]
 
     def make_move(self, allies, enemies):
+        for t in run.app.teams:
+            print(t[0].mage.__class__)
+            print(t[1].mage.__class__)
+            if t[0].mage.__class__ != t[1].mage.__class__:
+                for tt in t:
+                    tt.cur_hp = 0
         self.health = 0
         live = [e for e in enemies if e.health > 0]
         print(allies, enemies)
